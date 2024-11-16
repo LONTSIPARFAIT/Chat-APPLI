a85fc44dc46d312820b7d555f74d11538b6f8f4c
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2024-11-16 12:27:00 +0100

    version4

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import logo from '../assets/logo.png'
+import logo from '../assets/logo1.png'
 
 const AuthLayouts = ({children}) => {
   return (
     <>
-      <header className='flex justify-center items-center py-1 h-20 shadow-md '>
-        <img src={logo} alt='logo' height={100} width={200}/>
+      <header className='flex justify-center items-center py-4 h-20 shadow-md bg-white'>
+        <img src={logo} alt='logo'  height={100} width={75} className='rounded '/>
       </header>
 
       {children}
